6560717f536910d340580ae364344ff6c6ca2516
gabriella garcia <gabirvargas@gmail.com>
2023-04-20 22:45:13 -0300

    flats with css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # pagina principal
+  root to: "flats#index"
+  # pagina que mostra 1 apartamento
+  get "flats/:id", to: "flats#show", as: :flat
 end
